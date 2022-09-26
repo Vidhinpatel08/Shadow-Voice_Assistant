2ac550b09cdc361d36437664b3be8cddc8dc5605
Vidhinpatel08 <vidhin1208@gmail.com>
2022-09-26 14:09:20 +0530

    News Api

--- ShadowGui/features/mynewsapi.py
+++ ShadowGui/features/mynewsapi.py
@@ -10,31 +10,20 @@ def speak(str):
 
 def news():
     speak("News for today.. Lets begin")
-    # url = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=c16715a456f64335a3a7c582cbe2a202"
-    url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=c16715a456f64335a3a7c582cbe2a202"
 
-    news = requests.get(url).text
+    # today_url = f'https://newsapi.org/v2/everything?q=apple&from={year}-{month}-{day}&to={year}-{month}-{day}&sortBy=popularity&apiKey=c16715a456f64335a3a7c582cbe2a202'
+    # the-times-of-india = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=c16715a456f64335a3a7c582cbe2a202"
+    in_url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=c16715a456f64335a3a7c582cbe2a202"
+
+    news = requests.get(in_url).text
     news_dict = json.loads(news)
     arts = news_dict['articles']
-    for article in arts:
-        print(article['title'])
-        speak(article['title'])
-        speak("Moving on to the next news..Listen Carefully")
+    speak(f'We have Total {len(arts)} news.')
+    for i,article in enumerate(arts):
+        arg = f"News {i+1} is {article['title']}"
+        print(arg)
+        speak(arg)
 
-
-# from newsapi import NewsApiClient
-# def news():
-#     newsapi = NewsApiClient(api_key='c16715a456f64335a3a7c582cbe2a202')
-
-#     data = newsapi.get_top_headlines(q='fast and furious 9',
-#                                      language='en',
-#                                      page_size=5)
-
-#     newsdata = data['articles']
-#     for x, y in enumerate(newsdata):
-#         print(f'{x}{y["description"]}')
-#         speak((f'{x}{y["description"]}'))
-
-#     speak("that's it for now i'll update you in sometime")
-news()
+if __name__ == "__main__":
+    news()
 
